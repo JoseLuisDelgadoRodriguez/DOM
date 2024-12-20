ad73e6e1857fed5eb8beb43ba48606cad3f2d83f
José luis Delgado <luisdelgado25855@gmail.com>
2024-12-20 11:02:47 +0100

    clase part 2 20 12 24

--- Js/index.js
+++ Js/index.js
@@ -1,13 +1,13 @@
 function miclick(event) {
   event.target.style.color = "white";
   console.log(event.target.id);
-  document.getElementById(evento.target.id).style.backgroundColor = "red";
+  document.getElementById(event.target.id).style.backgroundColor = "red";
 
   if(event.ctrlKey == true) {
-    document.getElementById(evento.target.id).style.backgroundColor = "yellow";
+    document.getElementById(event.target.id).style.backgroundColor = "yellow";
   }
   if(event.altKey == true) {
-    document.getElementById(evento.target.id).style.backgroundColor = "blue";
+    document.getElementById(event.target.id).style.backgroundColor = "blue";
   }
 }
 function nogoogle(event) {
@@ -22,3 +22,13 @@ document.querySelector("#caja2").onclick = miclick;
 
 document.querySelector("#google").addEventListener("click", nogoogle);
 
+function miclick(event) {
+  console.log(event.target.id);
+
+
+  if(backgroundColor == "red") {
+    document.getElementById(event.target.id).style.backgroundColor = "blue";
+  }
+{else}{ "red"}
+
+}
